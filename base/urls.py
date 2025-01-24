1e075d7c298df77474b398a6c717f8a28b17a481
JuanJD07 <trespalaciojuan16@gmail.com>
2025-01-23 23:46:38 -0500

    ...

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from tkinter.font import names
+
+from django.urls import path
+from .views import ListaPendientes, DetalleTarea, CraerTarea, EditarTarea, EliminarTarea, Logueo, PaginaRegistro
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [path('', ListaPendientes.as_view(), name='tareas'),
+               path('login/', Logueo.as_view(), name='login'),
+               path('registro/', PaginaRegistro.as_view(), name='registro'),
+               path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+               path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea'),
+               path('crear-tarea/', CraerTarea.as_view(), name='crear-tarea'),
+               path('editar-tarea/<int:pk>', EditarTarea.as_view(), name='editar-tarea'),
+               path('eliminar-tarea/<int:pk>', EliminarTarea.as_view(), name='eliminar-tarea')]
